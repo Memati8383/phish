d646aa6d15c09cae6acf297b48578fd65b959c7b
Memati8383 <95960644+Memati8383@users.noreply.github.com>
2023-12-18 02:26:07 +0300

    Create ilk_animasyon.py

---
+++ ilk_animasyon.py
@@ -0,0 +1,9 @@
+# İlk animasyon
+def renkli_animasyon():
+    symbols = [Fore.RED + "█" + Fore.RESET, Fore.YELLOW + "█" + Fore.RESET, Fore.GREEN + "█" + Fore.RESET, Fore.BLUE + "█" + Fore.RESET]
+    
+    for i in range(1, 41):
+        sys.stdout.write('\r')
+        sys.stdout.write(''.join([symbols[(i + j) % len(symbols)] for j in range(40)]))
+        sys.stdout.flush()
+        time.sleep(0.2)
