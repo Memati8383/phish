a79034949b251d34e792a680c8a834524db57f9f
Memati8383 <95960644+Memati8383@users.noreply.github.com>
2023-12-17 22:46:51 +0300

    Create modül.py

---
+++ modül.py
@@ -0,0 +1,16 @@
+import os
+
+def modul_indir():
+  os.system("clear")
+  os.system("apt update && apt upgrade")
+  os.system("pkg install python")
+  os.system("pkg install python2")
+  os.system("pkg install python3")
+  os.system("pkg install pip")
+  os.system("pkg install pip2")
+  os.system("pkg install pip3")
+  os.system("pkg install git")
+  os.system("pip install colorama")
+  os.system("pip install requests")
+  os.system("chmod +x main.py")
+  os.system("clear")
